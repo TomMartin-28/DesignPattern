b290e2cd096997ed1c197dd22b8c3fb59f078981
Antoine Thomas <antoine.thomas654@gmail.com>
2021-04-05 00:10:26 +0200

    ajout d'une célébration pour bowser et mario

--- src/characters/Bowser.java
+++ src/characters/Bowser.java
@@ -2,9 +2,13 @@ package characters;
 
 import abilities.Ability;
 import abilities.SuperBowser;
+import strategie.celebrationVictoire.Arrogance;
 
 public class Bowser extends Character{
     public Bowser() {
         super("Bowser", 35, 10, new SuperBowser());
+        this.celebrationVictoire = new Arrogance();
     }
+    
+    
 }
--- src/characters/Mario.java
+++ src/characters/Mario.java
@@ -1,9 +1,11 @@
 package characters;
 
 import abilities.SuperMario;
+import strategie.celebrationVictoire.Modestie;
 
 public class Mario extends Character{
     public Mario(){
         super("Mario", 30, 7, new SuperMario());
+        this.celebrationVictoire = new Modestie();
     }
 }
